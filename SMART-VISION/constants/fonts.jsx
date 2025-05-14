a3fe16567f6a774760e68369a50dd5ab769a0441
santiago <santiagocarde.v06@gmail.com>
2025-05-14 15:46:19 -0500

    Arreglos en la optimizacion
    Cambio del lenguaje en el login y register

---
+++ SMART-VISION/constants/fonts.jsx
@@ -0,0 +1,8 @@
+export const FONTS = {
+    outfitRegular: 'Outfit_400Regular',
+    outfitMedium: 'Outfit_500Medium',
+    outfitSemiBold: 'Outfit_600SemiBold',
+    outfitBold: 'Outfit_700Bold',
+    outfitExtraBold: 'Outfit_800ExtraBold',
+  };
+  
