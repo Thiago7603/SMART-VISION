9ae4efb5154de997b69a3d68ef736fad6ece5c98
santiago <santiagocarde.v06@gmail.com>
2025-05-20 13:00:31 -0500

    Eliminacion de carpeta sin usar

--- SM1/constants/fonts.tsx
+++
@@ -1,8 +0,0 @@
-export const FONTS = {
-    outfitRegular: 'Outfit_400Regular',
-    outfitMedium: 'Outfit_500Medium',
-    outfitSemiBold: 'Outfit_600SemiBold',
-    outfitBold: 'Outfit_700Bold',
-    outfitExtraBold: 'Outfit_800ExtraBold',
-  };
-  
